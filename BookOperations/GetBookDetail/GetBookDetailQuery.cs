752fa72b39e7b6b1d5aa3ddd229980f6be1aa406
Ali İLMAN <aliilman48@gmail.com>
2024-01-09 13:37:02 +0300

    H4_3.ödev Model Kullanımı Çözüldü

--- BookOperations/GetBookDetail/GetBookDetailQuery.cs
+++ BookOperations/GetBookDetail/GetBookDetailQuery.cs
@@ -34,7 +34,7 @@ namespace WebApi.BookOperations.GetBookDetail
     {
         public string Title { get; set; }
         public int PageCount { get; set; }
-        public string PublishDate { get; set; }
+        public string? PublishDate { get; set; }
         public string Genre { get; set; }
     }
 }
